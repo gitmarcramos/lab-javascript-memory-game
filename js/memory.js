842b6017508d932d11073f8c001995e4157f853c
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-09-30 17:34:07 +0200

    finished the shuffled, working on the pairs

--- js/memory.js
+++ js/memory.js
@@ -1,14 +1,25 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    (this.pairsClicked = 0), (this.pairsGuessed = 0);
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(this.cards.length === 0) return undefined;
+    const newShuffledGame = [];
+
+    for (let i = 0; i < this.cards.length; i++) {
+      let dice = Math.floor(Math.random() * this.cards.length);
+      newShuffledGame.push(this.cards[dice]);
+    }
+    // console.log(this.cards);
+    // console.log(newShuffledGame);
+    return this.cards = newShuffledGame
   }
 
   checkIfPair(card1, card2) {
+    
     // ... write your code here
   }
 
